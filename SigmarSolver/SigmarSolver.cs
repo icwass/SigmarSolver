6ea8fef9517f84ead7e875c91ad07a54bc235c9a
icwass <isaac.wass@hotmail.com>
2024-04-11 22:53:49 -0500

    Initial commit

---
+++ SigmarSolver/SigmarSolver.cs
@@ -0,0 +1,43 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Quintessential;
+using Quintessential.Settings;
+using SDL2;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SigmarSolver;
+
+//using PartType = class_139;
+//using Permissions = enum_149;
+//using BondType = enum_126;
+//using BondSite = class_222;
+//using Bond = class_277;
+//using AtomTypes = class_175;
+//using PartTypes = class_191;
+//using Texture = class_256;
+public class MainClass : QuintessentialMod
+{
+	public static MethodInfo PrivateMethod<T>(string method) => typeof(T).GetMethod(method, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+	public override void Load()
+	{
+		//
+	}
+	public override void LoadPuzzleContent()
+	{
+		//
+	}
+	public override void Unload()
+	{
+		//
+	}
+	public override void PostLoad()
+	{
+		//
+	}
+}
